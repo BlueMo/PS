45e01508c9103721f85a4bef6b30ad3251f18c28
멘톳 <moon960323@gmail.com>
2020-08-17 02:56:05 +0000

    merge_sort_tree

---
+++ BOJ/13000-13999/13544.cpp
@@ -0,0 +1,70 @@
+// 13544: 수열과 쿼리 3
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int N, M, a, u, v, k, tree_size = 1, last_ans;
+vector<int> tree[1<<18];
+
+void updateST(int idx, int val)
+{
+	idx += tree_size;
+	tree[idx].push_back(val);
+	while (idx > 1)
+	{
+		idx >>= 1;
+		tree[idx].push_back(val);
+	}
+}
+
+int num_overK(int l, int r)
+{
+	int ans = 0;
+	l += tree_size, r += tree_size;
+	
+	while (l <= r)
+	{
+		if (l & 1)
+		{
+			ans += tree[l].end() - upper_bound(tree[l].begin(), tree[l].end(), k);
+			l++;
+		}
+		if (~r & 1)
+		{
+			ans += tree[r].end() - upper_bound(tree[r].begin(), tree[r].end(), k);
+			r--;
+		}
+		l >>= 1, r >>= 1;
+	}
+	return ans;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> N;
+	while (tree_size < N) tree_size <<= 1;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> a;
+		updateST(i, a);
+	}
+	cin >> M;
+	
+	for (int i = 1; i <= tree_size; ++i)
+		sort(tree[i].begin(), tree[i].end());
+	while (M--)
+	{
+		cin >> u >> v >> k;
+		u ^= last_ans;
+		v ^= last_ans;
+		k ^= last_ans;
+		last_ans = num_overK(u-1, v-1);
+		cout << last_ans << "\n";
+	}
+	return 0;
+}
