0604d4884dc8f26699d246cef9b4b8e96bdf996b
멘톳 <moon960323@gmail.com>
2020-08-17 05:46:25 +0000

    merge_sort_tree

---
+++ BOJ/13000-13999/13547.cpp
@@ -0,0 +1,75 @@
+// 13547: 수열과 쿼리 5
+
+#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+int N, Q, a[100001], tree_size = 1, u, v;
+unordered_map<int, int> map;
+vector<int> tree[1<<18];
+int next_idx[100001];
+
+void updateST(int idx, int val)
+{
+	idx += tree_size;
+	while (idx >= 1)
+	{
+		tree[idx].push_back(val);
+		idx >>= 1;
+	}
+}
+
+int num_overK(int l, int r, int val)
+{
+	int cnt = 0;
+	l += tree_size, r += tree_size;
+	
+	while (l <= r)
+	{
+		if (l & 1)
+		{
+			cnt += tree[l].end() - upper_bound(tree[l].begin(), tree[l].end(), val);
+			l++;
+		}
+		if (~r & 1)
+		{
+			cnt += tree[r].end() - upper_bound(tree[r].begin(), tree[r].end(), val);
+			r--;
+		}
+		l >>= 1, r >>= 1;
+	}
+	return cnt;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> N;
+	while (tree_size < N) tree_size <<= 1;
+	for (int i = 0; i < N; ++i)		// hashing
+	{
+		cin >> a[i];
+		if (map.end() == map.find(a[i])) map[a[i]] = map.size();
+		a[i] = map[a[i]];
+	}
+	for (int i = 0; i < map.size(); ++i) next_idx[i] = N;
+	for (int i = N-1; i >= 0; --i)
+	{
+		int &t = next_idx[a[i]];
+		updateST(i, t);
+		t = i;
+	}
+	for (int i = 1; i < tree_size; ++i) sort(tree[i].begin(), tree[i].end());
+	
+	cin >> Q;
+	while (Q--)
+	{
+		cin >> u >> v;
+		cout << num_overK(u-1, v-1, v-1) << "\n";
+	}
+	return 0;
+}
