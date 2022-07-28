54d50a4c575efb46df672ae7aff9abf9f9bb90d1
BlueMo <moon960323@gmail.com>
2022-07-28 21:52:21 +0900

    dp

--- BOJ/17000-17999/17070.cpp
+++ BOJ/17000-17999/17070.cpp
@@ -2,17 +2,27 @@
 #include <algorithm>
 using namespace std;
 
-int N, D[2], k;
+int N, a[20][20], D[20][20][3];
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
 
     cin >> N;
+    for (int i = 1; i <= N; i++) for (int j = 1; j <= N; j++) cin >> a[i][j];
 
-    D[1] = 1;
-    for (int i = 2; i <= N; i++, k = !k){
-        D[k] += D[!k];
+    D[1][2][0] = 1;
+    for (int i = 1; i <= N; i++){
+        for (int j = 1; j <= N; j++){
+            if (a[i][j]) continue;
+
+            D[i][j][0] += D[i][j - 1][0] + D[i][j - 1][2];
+            D[i][j][1] += D[i - 1][j][1] + D[i - 1][j][2];
+            if (!a[i - 1][j] && !a[i][j - 1]) D[i][j][2] += D[i - 1][j - 1][0] + D[i - 1][j - 1][1] + D[i - 1][j - 1][2];
+        }
     }
-    cout << D[!k];
+
+    cout << D[N][N][0] + D[N][N][1] + D[N][N][2];
+
+    return 0;
 }
